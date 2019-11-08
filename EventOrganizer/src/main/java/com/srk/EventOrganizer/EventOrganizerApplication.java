302621dd3049501b3c51d13b9cdd282b0334bfb7
shaishab <shaishab@MIDH3JSOED.mshome.net>
2019-11-08 10:47:31 +0530

    Added @CrossOrigin in EventController

--- EventOrganizer/src/main/java/com/srk/EventOrganizer/EventOrganizerApplication.java
+++ EventOrganizer/src/main/java/com/srk/EventOrganizer/EventOrganizerApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author shaishab
@@ -27,5 +31,13 @@ public class EventOrganizerApplication extends SpringBootServletInitializer{
 		// TODO Auto-generated method stub
 		return builder.sources(EventOrganizerApplication.class);
 	}
+	
+	/*
+	 * @Bean public WebMvcConfigurer corsConfigurer() { return new
+	 * WebMvcConfigurerAdapter() {
+	 * 
+	 * @Override public void addCorsMappings(CorsRegistry registry) {
+	 * registry.addMapping("/*").allowedOrigins("*"); } }; }
+	 */
 
 }
--- EventOrganizer/src/main/java/com/srk/EventOrganizer/controller/EventController.java
+++ EventOrganizer/src/main/java/com/srk/EventOrganizer/controller/EventController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import com.srk.EventOrganizer.service.EventService;
 
 @RestController
 @RequestMapping("/events")
+@CrossOrigin
 public class EventController {
 
 	@Autowired
@@ -52,6 +54,7 @@ public class EventController {
 	 * Get all the events from the MongoDB
 	 */
 	@GetMapping("/all")
+	//@CrossOrigin(origins = "http://localhost:4200")
 	public List<Event> getAllEvents(){
 		return eventService.getAllEvents();
 	}
@@ -60,6 +63,7 @@ public class EventController {
 	 * Get an event by specific eventId
 	 */
 	@GetMapping("/{eventId}")
+	//@CrossOrigin(origins = "http://localhost:4200")
 	public Optional<Event> getEventById(@PathVariable(name = "eventId") String eventId) {
 		return eventService.getEvent(eventId);
 	}
